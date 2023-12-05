a3882ba45b0fd9cb5dcef1fa80624a851b5256f8
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-12-05 08:51:51 -0700

    test: Add Market factory

---
+++ spec/factories/market.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :market do
+    name { Faker::Company.name }
+    street { Faker::Address.street_address }
+    city { Faker::Address.city }
+    county { Faker::Address.community }
+    state { Faker::Address.state }
+    zip { Faker::Address.zip }
+    lat { Faker::Address.latitude }
+    lon { Faker::Address.longitude }
+    vendor_count { Faker::Number.within(range: 1..50) }
+  end
+end
