50e8e06a46b8dad6dc47a0a693e7c44d1d045c46
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-12-05 22:56:02 -0700

    refactor: Consolidate VendorsController index to only require one variable

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -2,8 +2,7 @@ class Api::V0::VendorsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
 
   def index
-    market = Market.find(params[:market_id])
-    vendors = market.vendors
+    vendors = Market.find(params[:market_id]).vendors
     render json: VendorSerializer.new(vendors)
   end
 
