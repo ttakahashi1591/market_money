4e77b395a935f27cbfec331fa6353738ed3c1642
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-12-07 10:42:06 -0700

    test: Add MarketVendor delete test set-up

--- spec/requests/api/v0/market_vendor_request_spec.rb
+++ spec/requests/api/v0/market_vendor_request_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe "MarketVendors API Endpoints", type: :request do
       expect(created_market_vendor.vendor_id).to eq(market_vendor_params[:vendor_id])
     end
 
-    it "sends an error if invalid market id is provided" do
+    xit "sends an error if invalid market id is provided" do
       vendor = create(:vendor)
 
       market_vendor_params = {
@@ -81,6 +81,28 @@ RSpec.describe "MarketVendors API Endpoints", type: :request do
 
       expect(data[:errors]).to be_a(Array)
       expect(data[:errors].first[:detail]).to include("Vendor can't be blank")
+    end
+  end
+
+  describe "delete a marketvendor" do
+    xit "can delete an existing marketvendor" do
+      market = create(:market)
+      vendor = create(:vendor)
+
+      market_vendor_params = {
+                              market_id: market.id,
+                              vendor_id: vendor.id
+                              }
+      headers = { "CONTENT_TYPE" => "application/json" }
+
+      post "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
+      
+      market_vendor = MarketVendor.last
+
+      delete "/api/v0/market_vendors/#{market_vendor}"
+
+      expect(response).to have_http_status(201)
+      expect(response).to be_successful
     end
   end
 end
